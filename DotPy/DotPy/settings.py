1250bb6d7a0bbb50cb37b620fd31cb8ccc331e43
Einswilli <wilfriedgoeh@gmail.com>
2022-01-31 15:22:23 +0100

    configs for heroku

--- DotPy/DotPy/settings.py
+++ DotPy/DotPy/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'DotPy.urls'
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIR=[
     BASE_DIR/'static',
